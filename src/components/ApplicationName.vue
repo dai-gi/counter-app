de4c38940bb8f9cb91950951cd0d0f09596a99cd
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2023-07-05 19:52:43 +0900

    feat: アプリケーション名を表示するコンポーネントを追加

---
+++ src/components/ApplicationName.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ props.title }}</h1>
+</template>
+
+<script setup>
+
+  const props = defineProps({
+    title: String
+  })
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1 {
+    align-items: center;
+    display: flex;
+    justify-content: center;
+  }
+</style>
