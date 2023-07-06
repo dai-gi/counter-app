91c12404b7e9c70e2127d72d8fe5010ff484c0e4
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2023-07-06 19:09:33 +0900

    feat: カウント機能を追加

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <ApplicationName title="Counter App"/>
+  <CountFunction />
 </template>
 
 <script setup>
-import ApplicationName from './components/ApplicationName.vue'
+import ApplicationName from './components/ApplicationName.vue';
+import CountFunction from './components/CountFunction.vue';
 </script>
 
 <style>
---
+++ src/components/CountFunction.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="count-container">
+    <p class="count-content">{{ count }}</p>
+    <div class="count-btn-wrapper">
+      <div class="updown-btn-wrapper">
+        <button @click="increment" class="plus-btn btn">+</button>
+        <button @click="decrement" class="minus-btn btn">-</button>
+      </div>
+      <button @click="reset" class="reset-btn btn">クリア</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+
+  const count = ref(0);
+
+  const increment = () => {
+    count.value++;
+  }
+
+  const decrement = () => {
+    count.value--;
+  }
+
+  const reset = () => {
+    count.value = 0;
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .count-container {
+    display: grid;
+    justify-content: center;
+    align-content: center;
+    text-align: center;
+  }
+
+  .count-content {
+    border: 1px solid;
+    width: 200px;
+    padding: 10px 0;
+    font-size: 20px;
+  }
+
+  .updown-btn-wrapper {
+    display: flex;
+    justify-content: space-between;
+    padding: 20px 0;
+  }
+
+  .btn {
+    width: 80px;
+    height: 30px;
+  }
+
+  .plus-btn {
+    background: #2c8fff;
+    color: #fff;
+    font-size: 20px;
+    border: none;
+  }
+
+  .minus-btn {
+    background: #ff443b;
+    color: #fff;
+    font-size: 20px;
+    border: none;
+  }
+
+</style>
